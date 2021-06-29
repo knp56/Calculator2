e0a70a1b3e338a7dff83f613eff0ad43cc0f80cd
kimpadilla <knp56@njit.edu>
2021-06-28 21:13:54 -0400

    Feature: set up calculator with subtraction method

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,16 +1,20 @@
 def addition(a,b):
     return a + b
 
+def subtraction(a,b):
+    return a - b
 
 class Calculator:
     result = 0
 
     def __init__(self):
-        x = 2 + 2
-        self.result = x;
-        #pass
+        pass
 
     def add(self, a, b):
-        self.result = a + b
+        self.result = addition(a,b)
         #result = 0
-        return addition(a,b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = subtraction(a,b)
+        return self.result
