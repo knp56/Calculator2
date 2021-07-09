1feeaec1e27ca78c3424321451bc2913eac34529
kimpadilla <knp56@njit.edu>
2021-07-09 15:27:43 -0400

    Task: added setup text file

---
+++ src/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='Calculator',
+      version='0.0.1',
+      description='Its a Calculator',
+      author='Kim Padilla',
+      author_email='',
+      url='',
+      packages=find_packages(),
+      )
